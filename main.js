eacacd99c2e69fdd9354361f8b9ebe92f0f44ffd
Vanilladev23 <lamvip06192003@gmail.com>
2022-03-04 21:16:22 +0700

    chapter 7

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-// Break & Continue in loop
+// Vòng lặp lổng nhau - Nested loop
 
-for (let i = 0; i < 10; i++) {
-  if (i % 2 !== 0) {
-    continue;
+let myArray = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+
+for (let i = 0; i < myArray.length; i++) {
+  for (let j = 0; j < myArray[i].length; j++) {
+    console.log(myArray[i][j]);
   }
-  console.log(i);
 }
