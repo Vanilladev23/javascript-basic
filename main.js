ffe644787e0600d1895b862b4bdab95c20fa99ca
Vanilladev23 <lamvip06192003@gmail.com>
2022-03-05 12:24:14 +0700

    chapter 9

--- main.js
+++ main.js
@@ -1,5 +1,47 @@
-// Loop...
+/*
+  Array methods:
+  forEach()
+  every()
+  some()
+  find()
+  filter()
+  map()
+  reduce()
+*/
 
-for (let i = 0; i <= 100; i += 5) {
-  console.log(i);
-}
+let courses = [
+  {
+    id: 1,
+    name: 'Javascript',
+    coin: 250,
+  },
+  {
+    id: 2,
+    name: 'HTML,CSS',
+    coin: 0,
+  },
+  {
+    id: 3,
+    name: 'PHP',
+    coin: 250,
+  },
+  {
+    id: 4,
+    name: 'Python',
+    coin: 0,
+  },
+  {
+    id: 5,
+    name: 'Ruby',
+    coin: 250,
+  },
+  {
+    id: 6,
+    name: 'Ruby',
+    coin: 250,
+  },
+];
+let course = courses.filter(function (course, index) {
+  return course.name === 'Ruby';
+});
+console.log(course);
